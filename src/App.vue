af98150aede97d0e09e61f8923ce315114f246d6
liangdou <1368371032@qq.com>
2022-06-27 19:26:14 +0800

    首次提交：页面基本框架搭建

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
-
+<script>
+import { provide } from 'vue'
+import * as echarts from 'echarts'
+import axios from 'axios'
+export default {
+  setup(){
+    provide("echarts",echarts)
+    provide("axios",axios)
+  }
+}
+</script>
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  background: url("~@/assets/bg.jpg") top center no-repeat;
+  background-size: 100%;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
